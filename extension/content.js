2cadab23f7527e7f89d5c1e3b85fef05853461f9
jakejack13 <jckawesome13@gmail.com>
2019-03-30 13:05:04 -0400

    Started blocking function

--- extension/content.js
+++ extension/content.js
@@ -3,6 +3,8 @@ var imgSrcs = [];
 for (var i = 0; i < allImages.length; i ++) {
     console.log(allImages[i].src);
     imgSrcs.push(allImages[i].src);
+    allImages[i].style.display = "none";
+    console.log(allImages[i].getAttribute("display"));
 }
 var param = {imgs : imgSrcs}
 chrome.runtime.sendMessage(param);
