6cd3ba41c791ee366f72763d707ebf5c8d3da094
jakejack13 <jckawesome13@gmail.com>
2019-03-30 14:24:14 -0400

    Added an uneccesary method to Image because why not

--- extension/content.js
+++ extension/content.js
@@ -5,9 +5,11 @@ class Image {
     this.id = n;
     this.display = d;
   }
-
   unblock() {
     this.object.style.display = this.display;
+  }
+  block() {
+    this.object.style.display = "none";
   }
 }
 
@@ -18,7 +20,7 @@ for (var i = 0; i < allImages.length; i ++) {
     console.log(allImages[i].src);
     imgSrcs.push(allImages[i].src);
     imageObs.push(new Image(allImages[i],allImages[i].src,i,allImages[i].getAttribute("display")));
-    allImages[i].style.display = "none";
+    imageObs[i].block();
     console.log(allImages[i].getAttribute("display"));
 }
 var param = {imgs : imgSrcs}
